fd44aa110c1872725d62f1e19598fa1b2ac1cda0
krishnasai481 <orokrishna@gmail.com>
2026-04-05 18:03:16 +0530

    fix port

--- server.js
+++ server.js
@@ -1,68 +1,12 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-/// 🔥 CONNECT MONGODB (FIXED)
-mongoose.connect("mongodb://127.0.0.1:27017/unfrm")
+/// 🔥 CONNECT MONGODB (TEMP - LOCAL ONLY)
+//mongoose.connect("mongodb://127.0.0.1:27017/unfrm")
   .then(() => console.log("MongoDB Connected ✅"))
   .catch(err => console.log(err));
 
-/// 🔥 USER MODEL
-const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-});
+/// 🔥 PORT FIX (IMPORTANT FOR RENDER)
+const PORT = process.env.PORT || 5000;
 
-const User = mongoose.model("User", UserSchema);
-
-/// ✅ TEST ROUTE
-app.get("/", (req, res) => {
-  res.send("UNFRM Backend Running 🚀");
-});
-
-/// 🔥 SIGNUP
-app.post("/signup", async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const userExists = await User.findOne({ email });
-
-    if (userExists) {
-      return res.json({ success: false, message: "User exists" });
-    }
-
-    await User.create({ email, password });
-
-    res.json({ success: true });
-
-  } catch (error) {
-    res.json({ success: false, message: "Error creating user" });
-  }
-});
-
-/// 🔥 LOGIN
-app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  try {
-    const user = await User.findOne({ email, password });
-
-    if (user) {
-      res.json({ success: true });
-    } else {
-      res.json({ success: false });
-    }
-
-  } catch (error) {
-    res.json({ success: false });
-  }
-});
-
-app.listen(5000, "0.0.0.0",() => {
-  console.log("Server running on port 5000 🚀");
+app.listen(PORT, () => {
+  console.log("Server running 🚀 on port " + PORT);
 });
